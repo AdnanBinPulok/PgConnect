7c6724dc869566b51e971f8c676ec3e990e007b9
AdnanBinPulok <adnanbinpulok@gmail.com>
2025-02-28 16:02:29 +0600

    Bump version to 0.2.1 in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pgconnect',
-    version='0.2.0',
+    version='0.2.1',
     author='AdnanBinPulok',
     author_email='adnanbinpulok@gmail.com',
     description='A PostgreSQL connection and ORM library',
